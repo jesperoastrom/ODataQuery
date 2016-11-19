35bbb9a77b7316e185690d5b314395f51230f798
Jesper Åström <jesper.o.astrom@gmail.com>
2016-11-19 22:08:23 +0100

    Created karma config file for run once scenario. Added travis configuration file.

--- source/config/karma.ci.config.js
+++ source/config/karma.ci.config.js
@@ -2,5 +2,6 @@ var common = require('./karma.common.config');
 
 module.exports = function (config) {
     var karmaConfig = common.createCommonConfig(config);
+    karmaConfig.singleRun = true;
     config.set(karmaConfig);
 };
