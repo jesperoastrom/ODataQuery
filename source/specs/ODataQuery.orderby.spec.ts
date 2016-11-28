b0d37baa8ddc291550c558c98e4488fe9c297c21
Jesper Åström <jesper.o.astrom@gmail.com>
2016-11-28 18:21:54 +0100

    Refactored orderby spec

--- source/specs/ODataQuery.orderby.spec.ts
+++ source/specs/ODataQuery.orderby.spec.ts
@@ -2,32 +2,29 @@ import { ODataQuery } from '../ODataQuery';
 import { Person } from './ODataQuery.models.spec';
 
 describe('ODataQuery', () => {
-    let query: ODataQuery<Person>;
-
-    beforeEach(() => {
-        query = new ODataQuery<Person>();
-    });
-
     it('can order by simple property', () => {
-        expect(query
+        let query = new ODataQuery<Person>()
             .orderBy(person => person.name)
-            .toString())
-            .toBe('$orderby=name');
+            .toString();
+
+        expect(query).toBe('$orderby=name');
     });
 
     it('can order by complex property', () => {
-        expect(query
+        let query = new ODataQuery<Person>()
             .orderBy(person => person.mother.name)
-            .toString())
-            .toBe('$orderby=mother/name');
+            .toString();
+
+        expect(query).toBe('$orderby=mother/name');
     });
 
     it('can order by multiple properties', () => {
-        expect(query
+        let query = new ODataQuery<Person>()
             .orderBy(person => person.name)
             .orderByDesc(person => person.father.name)
             .orderBy(person => person.age)
-            .toString())
-            .toBe('$orderby=name,father/name desc,age');
+            .toString();
+
+        expect(query).toBe('$orderby=name,father/name desc,age');
     });
 });
